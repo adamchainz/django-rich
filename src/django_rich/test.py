8cd4fc846c274ba0ac28ea624ff04e7f0dcae1a9
Adam Johnson <me@adamj.eu>
2022-02-21 09:45:10 +0000

    Fix type check failures by adding annotations or ignore comments as appropriate - including two fixes to typeshed

--- src/django_rich/test.py
+++ src/django_rich/test.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import io
 import sys
 import unittest
+from types import TracebackType
+from typing import Union
+from unittest.case import TestCase
 from unittest.result import (  # type: ignore [attr-defined]
     STDERR_LINE,
     STDOUT_LINE,
@@ -21,12 +25,21 @@ if django.VERSION >= (3, 0):
     from django.test.runner import PDBDebugResult
 
 
+_SysExcInfoType = Union[
+    tuple[type[BaseException], BaseException, TracebackType], tuple[None, None, None]
+]
+
+
 class RichTextTestResult(unittest.TextTestResult):
+    # Declaring attribute since typeshed had wrong capitalization
+    # https://github.com/python/typeshed/pull/7340
+    showAll: bool
+
     console = Console(stderr=True)
     django_green = Style(color=Color.from_rgb(32, 170, 118))
 
     @failfast
-    def addError(self, test, err):
+    def addError(self, test: TestCase, err: _SysExcInfoType) -> None:
         self.errors.append((test, self._exc_info_to_string(err, test)))
         self._mirrorOutput = True
         if self.showAll:
@@ -34,14 +47,14 @@ class RichTextTestResult(unittest.TextTestResult):
         elif self.dots:
             self.console.print("E", style=Style(color="red"), end="")
 
-    def addSuccess(self, test):
+    def addSuccess(self, test: TestCase) -> None:
         if self.showAll:
             self.console.print("ok", style=self.django_green)
         elif self.dots:
             self.console.print(".", style=self.django_green, end="")
 
     @failfast
-    def addFailure(self, test, err):
+    def addFailure(self, test: TestCase, err: _SysExcInfoType) -> None:
         self.failures.append((test, self._exc_info_to_string(err, test)))
         self._mirrorOutput = True
         if self.showAll:
@@ -49,14 +62,14 @@ class RichTextTestResult(unittest.TextTestResult):
         elif self.dots:
             self.console.print("F", style=Style(color="red"), end="")
 
-    def addSkip(self, test, reason):
+    def addSkip(self, test: TestCase, reason: str) -> None:
         self.skipped.append((test, reason))
         if self.showAll:
             self.console.print(f"skipped {reason!r}", style=Style(color="yellow"))
         elif self.dots:
             self.console.print("s", style=Style(color="yellow"), end="")
 
-    def addExpectedFailure(self, test, err):
+    def addExpectedFailure(self, test: TestCase, err: _SysExcInfoType) -> None:
         self.expectedFailures.append((test, self._exc_info_to_string(err, test)))
         if self.showAll:
             self.console.print("expected failure", style=Style(color="yellow"))
@@ -64,18 +77,18 @@ class RichTextTestResult(unittest.TextTestResult):
             self.console.print("x", style=Style(color="yellow"), end="")
 
     @failfast
-    def addUnexpectedSuccess(self, test):
+    def addUnexpectedSuccess(self, test: TestCase) -> None:
         self.unexpectedSuccesses.append(test)
         if self.showAll:
             self.console.print("unexpected success", style=Style(color="red"))
         elif self.dots:
             self.console.print("u", style=Style(color="red"), end="")
 
-    def _exc_info_to_string(self, err, test):
+    def _exc_info_to_string(self, err: _SysExcInfoType, test: TestCase) -> str:
         """Converts a sys.exc_info()-style tuple of values into a string."""
         exctype, value, tb = err
         # Skip test runner traceback levels
-        while tb and self._is_relevant_tb_level(tb):
+        while tb and self._is_relevant_tb_level(tb):  # type: ignore [attr-defined]
             tb = tb.tb_next
 
         extract = Traceback.extract(exctype, value, tb, show_locals=True)
@@ -84,7 +97,9 @@ class RichTextTestResult(unittest.TextTestResult):
             self.console.print(tb_e)
         msgLines = capture.get()
         if self.buffer:
+            assert isinstance(sys.stdout, io.StringIO)
             output = sys.stdout.getvalue()
+            assert isinstance(sys.stderr, io.StringIO)
             error = sys.stderr.getvalue()
             if output:
                 if not output.endswith("\n"):
@@ -96,12 +111,18 @@ class RichTextTestResult(unittest.TextTestResult):
                 msgLines.append(STDERR_LINE % error)
         return "".join(msgLines)
 
-    def printErrorList(self, flavour, errors):
+    # Typeshed had wrong signature
+    # https://github.com/python/typeshed/pull/7341
+    def printErrorList(  # type: ignore [override]
+        self,
+        flavour: str,
+        errors: list[tuple[TestCase, str]],
+    ) -> None:
         for test, err in errors:
             r = Rule(style=self.django_green)
             title = f"{flavour}: {self.getDescription(test)}"
             self.console.print(r, title, r)
-            self.stream.writeln("%s" % err)
+            self.stream.write("%s\n" % err)
 
 
 class RichDebugSQLTextTestResult(DebugSQLTextTestResult, RichTextTestResult):
@@ -121,8 +142,9 @@ class RichTestRunner(DiscoverRunner.test_runner):
 class RichRunner(DiscoverRunner):
     test_runner = RichTestRunner
 
-    def get_resultclass(self):
+    def get_resultclass(self) -> type[RichTextTestResult] | None:
         if self.debug_sql:
             return RichDebugSQLTextTestResult
         elif django.VERSION >= (3, 0) and self.pdb:
             return RichPDBDebugResult
+        return None
