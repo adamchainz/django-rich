2846a0f5852ff1efd5ee1ee691e29486cae313e3
Adam Johnson <me@adamj.eu>
2024-08-11 22:50:48 +0100

    Simpler test

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 import sys
 from code import InteractiveConsole
-from textwrap import dedent
 from unittest import mock
 
 import pytest
 from django.core.management import call_command
 from django.test import SimpleTestCase
-from django.test.utils import captured_stdin, captured_stdout
+from django.test.utils import captured_stdin
+from django.test.utils import captured_stdout
 
 
 class ShellCommandTestCase(SimpleTestCase):
@@ -20,29 +20,12 @@ class ShellCommandTestCase(SimpleTestCase):
     def test_pretty(self, select):
         console = InteractiveConsole()
         with captured_stdin() as _, captured_stdout() as stdout:
-            call_command("shell")
-            console.push(
-                dedent(
-                    """
-                    test = {
-                        "foo": [1, 2, 3, (4, 5, {6}, 7, 8, {9}), {}],
-                        "bar": {"egg": "baz", "words": ["Hello World"] * 10},
-                        False: "foo",
-                        True: "",
-                        "text": ("Hello World", "foo bar baz egg"),
-                    }
-                    """
-                )
-            )
-            console.push("test\n")
-        assert (
-            stdout.getvalue().strip()
-            == "{\n    'foo': [1, 2, 3, (4, 5, {6}, 7, 8, {9}), {}],\n    'bar':"
-            " {\n        'egg': 'baz',\n        'words': [\n            'Hello"
-            " World',\n            'Hello World',\n            'Hello World',\n"
-            "            'Hello World',\n            'Hello World',\n          "
-            "  'Hello World',\n            'Hello World',\n            'Hello "
-            "World',\n            'Hello World',\n            'Hello World'\n  "
-            "      ]\n    },\n    False: 'foo',\n    True: '',\n    'text':"
-            " ('Hello World', 'foo bar baz egg')\n}"
-        )
+            call_command("shell", "-i", "python")
+            console.push("from rich.panel import Panel\n")
+            console.push('Panel.fit("hi!")\n')
+        lines = stdout.getvalue().splitlines()
+        assert lines == [
+            "╭─────╮",
+            "│ hi! │",
+            "╰─────╯",
+        ]
