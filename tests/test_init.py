752c6c6cb96d7cf12cdcba741d2f98c60ce00e3e
Adam Johnson <me@adamj.eu>
2024-10-20 13:05:36 +0100

    Drop faker and factory boy from tests, unnecessary

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,32 +1,19 @@
 from __future__ import annotations
 
-import faker
 from django.db.models import Sum
 from django.db.models.functions import Length
 from django.test import TestCase
 from django.test.utils import captured_stdout
-from factory import lazy_attribute
-from factory.django import DjangoModelFactory
 
 from django_rich import tabulate
 
 from .testapp.models import Person
 
-faker = faker.Factory.create()
-
-
-class PersonFactory(DjangoModelFactory):  # type: ignore [misc]
-    class Meta:
-        model = "testapp.Person"
-
-    name = lazy_attribute(lambda o: faker.name())  # pragma: no cover
-    age = lazy_attribute(lambda o: faker.random_number(digits=2))  # pragma: no cover
-
 
 class TabulateTests(TestCase):
     def test_queryset(self):
-        PersonFactory(name="Test Name", age=8)
-        PersonFactory(name="Another Name", age=88)
+        Person.objects.create(name="Test Name", age=8)
+        Person.objects.create(name="Another Name", age=88)
         with captured_stdout() as stdout:
             tabulate(Person.objects.all())
         lines = stdout.getvalue().splitlines()
@@ -41,8 +28,8 @@ class TabulateTests(TestCase):
         ]
 
     def test_elided(self):
-        PersonFactory(name="Test Name", age=8)
-        PersonFactory(name="Another Name", age=88)
+        Person.objects.create(name="Test Name", age=8)
+        Person.objects.create(name="Another Name", age=88)
         with captured_stdout() as stdout:
             tabulate(Person.objects.all(), limit=1)
         lines = stdout.getvalue().splitlines()
@@ -60,15 +47,15 @@ class TabulateTests(TestCase):
         ]
 
     def test_single_value(self):
-        PersonFactory(name="Test Name", age=8)
+        Person.objects.create(name="Test Name", age=8)
         with captured_stdout() as stdout:
             tabulate(Person.objects.count())
         lines = stdout.getvalue().splitlines()
         assert lines == ["1"]
 
     def test_dict(self):
-        PersonFactory(name="Test Name", age=8)
-        PersonFactory(name="Another Name", age=88)
+        Person.objects.create(name="Test Name", age=8)
+        Person.objects.create(name="Another Name", age=88)
         with captured_stdout() as stdout:
             tabulate(Person.objects.aggregate(Sum("age")))
         lines = stdout.getvalue().splitlines()
@@ -81,8 +68,8 @@ class TabulateTests(TestCase):
         ]
 
     def test_annotation(self):
-        PersonFactory(name="Test Name", age=8)
-        PersonFactory(name="Another Name", age=88)
+        Person.objects.create(name="Test Name", age=8)
+        Person.objects.create(name="Another Name", age=88)
         with captured_stdout() as stdout:
             tabulate(Person.objects.values("name").annotate(len=Length("age")))
         lines = stdout.getvalue().splitlines()
@@ -102,7 +89,7 @@ class TabulateTests(TestCase):
         assert stdout.getvalue() == "\n"
 
     def test_only(self):
-        PersonFactory(name="Test Name", age=8)
+        Person.objects.create(name="Test Name", age=8)
         with captured_stdout() as stdout:
             tabulate(Person.objects.all().only("age"))
         lines = stdout.getvalue().splitlines()
@@ -117,7 +104,7 @@ class TabulateTests(TestCase):
         ]
 
     def test_defer(self):
-        PersonFactory(name="Test Name", age=8)
+        Person.objects.create(name="Test Name", age=8)
         with captured_stdout() as stdout:
             tabulate(Person.objects.all().defer("age"))
         lines = stdout.getvalue().splitlines()
